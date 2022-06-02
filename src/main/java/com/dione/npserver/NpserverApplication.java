c1bd23ff73acccc1fd4b8057422f4910c8933528
Michele de Meester <demeestermichele@gmail.com>
2022-06-02 11:38:59 +0200

    init commit

---
+++ src/main/java/com/dione/npserver/NpserverApplication.java
@@ -0,0 +1,21 @@
+package com.dione.npserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class NpserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NpserverApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name){
+        return String.format("Hello %s!", name);
+    }
+}
---
+++ src/test/java/com/dione/npserver/NpserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dione.npserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
