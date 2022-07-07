94afaf21578512d8928bfff0562766ba7bc5eed3
Michele de Meester <demeestermichele@gmail.com>
2022-07-07 12:14:20 +0200

    ChapterRepository first draft

--- src/main/java/com/dione/npserver/repository/ChapterRepository.java
+++ src/main/java/com/dione/npserver/repository/ChapterRepository.java
@@ -1,4 +1,17 @@
 package com.dione.npserver.repository;
 
-public interface ChapterRepository {
+import com.dione.npserver.model.Chapter;
+import com.dione.npserver.model.Character;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Set;
+
+public interface ChapterRepository extends CrudRepository<Chapter, Integer> {
+
+    public Chapter findChapterById(Integer id);
+    public List<Chapter> findChaptersByNameContaining(String name);
+    public Chapter findChaptersByNumber(float number);
+    public List<Chapter> findChaptersByCharactersListContaining(List<Character> charactersList);
+
 }
