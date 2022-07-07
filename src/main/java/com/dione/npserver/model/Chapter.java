b69660ad9590901ad0f2317bce6cfe399c8e29d0
Michele de Meester <demeestermichele@gmail.com>
2022-07-07 12:15:14 +0200

    Chapter model new variables

--- src/main/java/com/dione/npserver/model/Chapter.java
+++ src/main/java/com/dione/npserver/model/Chapter.java
@@ -13,26 +13,32 @@ public class Chapter implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private String chapterName;
+    private String name;
+
+    private float number;
+
+    private String description;
 
     @ManyToMany
     @JoinTable(
             name = "chapter_characters",
             joinColumns = @JoinColumn(name = "character_id"),
             inverseJoinColumns = @JoinColumn(name = "chapter_id"))
-    private Set<Character> charactersList;
+    private List<Character> charactersList;
 
     /**Constructors**/
     public Chapter() {
-
     }
 
-    public Chapter(String chapterName) {
-        this.chapterName = chapterName;
+    public Chapter(Long id) {
+        this.id = id;
     }
 
-    public Chapter(Long id, String chapterName, Set<Character> charactersList) {
-        this.chapterName = chapterName;
+    public Chapter(Long id, String name, float number, String description, List<Character> charactersList) {
+        this.id = id;
+        this.name = name;
+        this.number = number;
+        this.description = description;
         this.charactersList = charactersList;
     }
 
@@ -45,27 +51,46 @@ public class Chapter implements Serializable {
         this.id = id;
     }
 
-    public String getChapterName() {
-        return chapterName;
+    public String getName() {
+        return name;
     }
 
-    public void setChapterName(String chapterName) {
-        this.chapterName = chapterName;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public Set<Character> getCharactersList() {
+    public float getNumber() {
+        return number;
+    }
+
+    public void setNumber(float number) {
+        this.number = number;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Character> getCharactersList() {
         return charactersList;
     }
 
-    public void setCharactersList(Set<Character> charactersList) {
+    public void setCharactersList(List<Character> charactersList) {
         this.charactersList = charactersList;
     }
+
     /**To String**/
     @Override
     public String toString() {
         return "Chapter{" +
                 "id=" + id +
-                ", chapterName='" + chapterName + '\'' +
+                ", name='" + name + '\'' +
+                ", number=" + number +
+                ", description='" + description + '\'' +
                 ", charactersList=" + charactersList +
                 '}';
     }
