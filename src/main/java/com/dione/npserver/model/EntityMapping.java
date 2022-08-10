59c697e3a0bc5cd622b762f84d8dc508edd43715
Dione <demeestermichele@gmail.com>
2022-08-10 14:41:35 +0200

    Entity model first draft

--- src/main/java/com/dione/npserver/model/EntityMapping.java
+++ src/main/java/com/dione/npserver/model/EntityMapping.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.List;
-import java.util.Set;
 
 @Entity
 @Table(name = "Entity_mapping")
